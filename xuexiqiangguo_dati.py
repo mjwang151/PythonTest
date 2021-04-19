bea1d45904e1a488ac72dd2af6f13fc7a5f6fff6
mjwang <15151552733@163.com>
2021-04-19 17:32:56 +0800

    tijiao

--- xuexiqiangguo_dati.py
+++ xuexiqiangguo_dati.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
 # 打开浏览器
@@ -28,32 +29,28 @@ time.sleep(2)
 titleCount = int(browser.find_elements_by_class_name('pager')[0].text.split('/')[1])
 for i in range(1, 6):
     print('开始回答第' + str(i) + '道题目。')
-    time.sleep(1)
-    browser.find_elements_by_class_name('tips')[0].click()
+    time.sleep(2)
+    tiMuType = browser.find_elements_by_class_name('q-header')[0].text
     # 点击显示提示信息
-    browser.find_elements_by_class_name('line-feed')
-    browser.find_elements_by_class_name('line-feed')[0].find_elements_by_xpath("//font")
+    browser.find_elements_by_class_name('tips')[0].click()
+    time.sleep(2)
     tips_tmp = []
     tips = browser.find_elements_by_class_name('line-feed')[0].find_elements_by_xpath("//font")
     for ji in range(len(browser.find_elements_by_class_name('line-feed')[0].find_elements_by_xpath("//font"))):
         print(str(ji) + ':' + str(len(browser.find_elements_by_class_name('line-feed')[0].find_elements_by_xpath("//font"))))
         tips_tmp.append(browser.find_elements_by_class_name('line-feed')[0].find_elements_by_xpath("//font")[ji].text)
-    # for tip in tips:
-    #     tips_tmp.append(tip.text)
-    #     print('提示信息:' + tip.text)
     print('缓存到的提示信息:' + str(tips_tmp))
-    time.sleep(1)
-    # 点击隐藏提示信息
     tips_msg = browser.find_elements_by_class_name('line-feed')[0].text
+    time.sleep(2)
+    # 点击隐藏提示信息
     browser.find_elements_by_class_name('tips')[0].click()
-    tiMuType = browser.find_elements_by_class_name('q-header')[0].text
+    time.sleep(2)
     if tiMuType == '多选题':
         print('此题为多选题:')
         ansCount = 0
         xXian = browser.find_elements_by_class_name('q-answer')
         for xx in xXian:
             for tip in tips_tmp:
-                print('========多选题答案是:' + xx.text + '，提示为：' + tip)
                 if tip == xx.text[3:]:
                     print('多选题答案是:' + xx.text + '，提示为：' + tip)
                     xx.click()
@@ -90,4 +87,4 @@ for i in range(1, 6):
     isYesVal = browser.find_elements_by_class_name('answer')
     if len(isYesVal) > 0:
         print('此题答错，正确答案为：' + browser.find_elements_by_class_name('answer')[0].text)
-        # browser.find_elements_by_class_name('ant-btn')[0].click()
+        browser.find_elements_by_class_name('ant-btn')[0].click()
